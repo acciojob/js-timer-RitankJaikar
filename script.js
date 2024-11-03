34757d23c3530fcc1f472d257db4e31f1b2c72ca
Ritank Jaikar <ritank.jaikar@gmail.com>
2024-11-03 14:52:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 // write js code here if required
+const timer= document.querySelector("#timer");
+
+setInterval(() => {
+	const time= new Date();
+	const options = {
+	  day: '2-digit',
+	  month: '2-digit',
+	  year: 'numeric',
+	  hour: '2-digit',
+	  minute: '2-digit',
+	  second: '2-digit'
+	};
+	timer.innerText= time.toLocaleString('en-GB', options);
+}, 1000)
